67bcee4e091a187ee697728ebb9dce5599cd8a5f
Reioo <reio.meiusi@gmail.com>
2023-01-05 21:38:33 +0200

    Add function playerSelection

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 console.log("Hello");
-let rockCount = 0;
 function getComputerChoice() {
     var choice = ['Rock', 'Paper', 'Sicors'],
     computerChoice = choice[Math.floor(Math.random() * choice.length)];
-    console.log(computerChoice);
+    // console.log(computerChoice);
+}
+function playerSelection() {
+    let keepGoing = true;
+    while (keepGoing === true) {
+        let playerChoice = prompt('Rock / Paper / Sicors: ');
+        playerChoice = playerChoice.toLowerCase();
+        console.log(playerChoice);
+        if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'sicors') {
+            keepGoing = false;
+        } else {
+            console.log('Please try again');
+        }
+    }
 }
 getComputerChoice();
+playerSelection();
+console.clear;

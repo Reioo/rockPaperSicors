1fe9ef5c592c5bbe4f74357c99e913884cbab77a
Reioo <reio.meiusi@gmail.com>
2023-01-05 22:29:09 +0200

    Add game function with 5 rounds

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+
 function getComputerChoice() {
     var choice = ['rock', 'paper', 'sicors'],
     computerChoice = choice[Math.floor(Math.random() * choice.length)];
@@ -8,7 +8,7 @@ function playerSelection() {
     let keepGoing = true;
     let playerChoice;
     while (keepGoing === true) {
-        playerChoice = prompt('Rock / Paper / Sicors: ');
+        playerChoice = prompt('Choose: Rock / Paper / Sicors: ');
         playerChoice = playerChoice.toLowerCase();
         if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'sicors') {
             keepGoing = false;
@@ -21,20 +21,45 @@ function playerSelection() {
 function singleRound() {
     let humanChoice = playerSelection();
     let computerChoice = getComputerChoice();
-    
+    let winner;
     console.log('Player: ' + humanChoice);
     console.log('Computer: ' + computerChoice);
-    checkWinner(humanChoice, computerChoice);
+    winner = checkWinner(humanChoice, computerChoice);
+
+    return winner;
 }
 function checkWinner(human, computer) {
+    let winner;
     if (human === computer) {
         console.log("It's a tie");
     }
     else if (human == 'rock' && computer == 'sicors' || human == 'sicors' && computer == 'paper' || human == 'paper' && computer == 'rock') {
         console.log('You win!! ' + human + ' beats ' + computer);
+        winner = 'player';
     } else { 
         console.log('Computer wins! ' + computer + ' beats ' + human);
+        winner = 'computer';
+    }
+    return winner;
+
+}
+function game() {
+    let playerWinCount = 0;
+    let computerWinCount = 0;
+    let tieCount = 0;
+    let roundCount = 1;
+    for (let i = 0 ; i < 5 ; i++) {
+        let winner = singleRound();
+        if (winner == 'player') {
+            playerWinCount++;
+        } else if (winner == 'computer') {
+            computerWinCount++;
+        }
+        console.log("Player: " + playerWinCount);
+        console.log("Computer: " + computerWinCount);
+        console.log('_________________ROUND ' +roundCount+ ' ________________');
+        roundCount++;
     }
 }
-singleRound();
+game();
 console.clear;
